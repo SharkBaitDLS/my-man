b4bd54fbd6a331b24ca8a47e5182d88b9bccbf92
Isaac M. Good <imgood@imgoodproductions.org>
2020-05-13 03:04:58 -0700

    Rewrite to support DMs and better unify message-based channel discovery

--- src/main.rs
+++ src/main.rs
@@ -57,26 +57,6 @@ fn list<'a, 'b>(msg: &'a mut CreateMessage<'b>) -> &'a mut CreateMessage<'b> {
    return msg.content(list_message + "```");
 }
 
-fn stop(ctx: &Context, msg: &Message) {
-   let guild = match msg.guild(&ctx.cache) {
-      Some(guild) => guild,
-      None => {
-         log_on_error(msg.author.direct_message(ctx, |m| m.content(
-            "I don't know what guild to join when you DM me, please write in a chat channel.")));
-         return;
-      }
-   };
-   let guild_id = guild.read().id;
-
-   let manager_lock = ctx.data.read().get::<VoiceManager>().cloned().expect("Expected VoiceManager in data map.");
-   let mut manager = manager_lock.lock();
-
-   match manager.get_mut(guild_id) {
-      Some(handler) => handler.stop(),
-      None => warn!("Could not load audio handler to stop.") 
-   };
-}
-
 fn join_and_play(
    ctx: &Context,
    guild_id: GuildId,
@@ -99,55 +79,62 @@ fn join_and_play(
    };
 }
 
-fn join_message_and_play(ctx: &Context, msg: &Message, source: Box<dyn voice::AudioSource>, volume: f32) {
-   let guild = match msg.guild(&ctx.cache) {
-      Some(guild) => guild,
-      None => {
-         log_on_error(msg.author.direct_message(ctx, |m| m.content(
-            "I don't know what guild to join when you DM me, please write in a chat channel.")));
-         return;
-      }
-   };
-
-   let guild_id = guild.read().id;
-   let channel_id = guild.read().voice_states.get(&msg.author.id).and_then(|voice_state| voice_state.channel_id);
-
-   let connect_to = match channel_id {
-      Some(channel) => channel,
-      None => {
-         log_on_error(msg.author.direct_message(ctx, |m| m.content("You are not in a voice channel!")));
-         return;
-      }
-   };
-
-   join_and_play(ctx, guild_id, connect_to, source, volume);
+struct ConnectionData {
+   guild: GuildId,
+   channel: ChannelId
 }
 
-fn join_message(ctx: &Context, msg: &Message) {
-   let guild = match msg.guild(&ctx.cache) {
-      Some(guild) => guild,
-      None => {
-         log_on_error(msg.author.direct_message(ctx, |m| m.content(
-            "I don't know what guild to join when you DM me, please write in a chat channel.")));
-         return;
-      }
+fn get_connection_data_from_message(ctx: &Context, msg: &Message) -> Option<ConnectionData> {
+   let possible_guilds = match msg.guild(&ctx.cache) {
+      Some(guild) => vec![guild],
+      None => ctx.cache.read().user.guilds(&ctx.http).unwrap_or_else(|err| {
+         error!("Error retrieving this bot's guilds: {}", &err);
+         return Vec::new();
+      }).into_iter().filter_map(|info| info.id.to_guild_cached(&ctx.cache)).collect()
    };
 
-   let guild_id = guild.read().id;
-   let channel_id = guild.read().voice_states.get(&msg.author.id).and_then(|voice_state| voice_state.channel_id);
+   return possible_guilds.into_iter()
+      .find_map(|guild| match guild.read().voice_states.get(&msg.author.id).and_then(|state| state.channel_id) {
+         Some(channel_id) => Some(ConnectionData { guild: guild.read().id, channel: channel_id }),
+         None => None
+      });
+}
 
-   let connect_to = match channel_id {
-      Some(channel) => channel,
-      None => {
-         log_on_error(msg.author.direct_message(ctx, |m| m.content("You are not in a voice channel!")));
-         return;
-      }
+macro_rules! get_connection_data_or_return {
+   ($ctx:ident, $msg:ident) => {
+      match get_connection_data_from_message($ctx, $msg) {
+         Some(data) => data,
+         None => {
+            log_on_error($msg.author.direct_message($ctx, |m| m.content("You are not in a voice channel!")));
+            return;
+         }
+      };
    };
+}
+
+fn stop(ctx: &Context, msg: &Message) {
+   let connect_to = get_connection_data_or_return!(ctx, msg);
 
    let manager_lock = ctx.data.read().get::<VoiceManager>().cloned().expect("Expected VoiceManager in data map.");
    let mut manager = manager_lock.lock();
 
-   match manager.join(guild_id, connect_to) {
+   match manager.get_mut(connect_to.guild) {
+      Some(handler) => handler.stop(),
+      None => warn!("Could not load audio handler to stop.") 
+   };
+}
+
+fn join_message_and_play(ctx: &Context, msg: &Message, source: Box<dyn voice::AudioSource>, volume: f32) {
+   let connect_to = get_connection_data_or_return!(ctx, msg);
+   join_and_play(ctx, connect_to.guild, connect_to.channel, source, volume);
+}
+
+fn join_message(ctx: &Context, msg: &Message) {
+   let connect_to = get_connection_data_or_return!(ctx, msg);
+   let manager_lock = ctx.data.read().get::<VoiceManager>().cloned().expect("Expected VoiceManager in data map.");
+   let mut manager = manager_lock.lock();
+
+   match manager.join(connect_to.guild, connect_to.channel) {
       Some(_) => (),
       None => error!("Could not load audio handler for playback.") 
    };
